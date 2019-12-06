5b112e30f1c3fe9776a83e525f000d5d5980ccff
eggs <eggs@T480-CHENWEI>
2019-12-06 11:58:13 +0800

    添加项目文件。

---
+++ Wolfdog.Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+using System;
+
+namespace Wolfdog.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String QueuesNameESF = "queue://test.log";
+            Uri _uri = new Uri(String.Concat("activemq:tcp://127.0.0.1:61616"));
+            IConnectionFactory factory = new ConnectionFactory(_uri);
+            using (IConnection conn = factory.CreateConnection("admin", "manager"))
+            {
+                using (ISession session = conn.CreateSession())
+                {
+                    IDestination destination = SessionUtil.GetDestination(session, QueuesNameESF);
+                    using (IMessageProducer producer = session.CreateProducer(destination))
+                    {
+                        conn.Start();
+                        //可以写入字符串，也可以是一个xml字符串等
+                        ITextMessage request = session.CreateTextMessage("messsage");
+                        producer.Send(request);
+
+                    }
+                }
+            }
+
+        }
+    }
+}
